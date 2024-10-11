f31cb315726508feba26db91d74fd8eb67632c26
Michelle Zhang <mizhang@chapman.edu>
2024-10-10 17:52:48 -0700

    Fill in content and work on buttons

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import Image from 'next/image';
 
 const HeroSection = () => {
+  const name = 'Michelle Zhang';
+  const role = 'Software Engineer';
+  const bioIntro = `Hey there! My name is Michelle Zhang and I am a software engiener!
+    I graduated from Chapman University in May 2024 with a B.S. in Computer Science and a minor in Data Analytics.`;
+  const bioBackground = 'I have a background in Full-Stack Developing, Web Development, and Data Analytics.';
+
   return (
     <section>
-        <div className='grid grid-cols-1 lg:grid-cols-12'>
-            <div className='col-span-7 place-self-center'>
-                <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-                    Hello, I'm Michelle!
-                </h1>
-                <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
-                    Dummy text dummy text  dummy text  dummy text  dummy text  dummy text
-                </p>
-                <div>
-                    <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-                    <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
-                </div>
-            </div>
-            <div className='col-span-5 place-self-center mt-4 lg:m-0'>
-                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                    <Image
-                        src='/images/profile.png'
-                        alt='hero image'
-                        className='absolute transform -translate-x-1/2 translate-y-1/2 top-1/2 left-1/2'
-                        width={300}
-                        height={300}
-                    />
-                </div>
-            </div>
+      <div className='grid grid-rows-1 lg:grid-rows-3'>
+        <div className='row-span-2 place-self-center mt-4 lg:m-0'>
+          <Image
+            src='/images/profile.png'
+            alt='hero image'
+            className='absolute transform -translate-x-1/2 translate-y-1/2 top-1/2 left-1/2'
+            width={300}
+            height={300}
+          />
         </div>
+        <div className='col-span-7 place-self-center'>
+          <h1 className='text-black mb-4 text-4xl lg:text-6xl font-extrabold'>{name}</h1>
+          <h3 className='text-black text-lg mb-6 lg:text-xl'>{role}</h3>
+          <div>
+            {/* TODO: make background color transition smoother */}
+            <button className='px-6 py-3 rounded-full mr-4 bg-[#0050FF] text-white border border-[#0050FF] hover:bg-white hover:text-black hover:border-black transition-all hover:delay-200'>Resume</button>
+            <button className='px-6 py-3 rounded-full bg-transparent text-black border border-black mt-3 hover:bg-[#0050FF] hover:text-white hover:border-transparent transition-all hover:delay-200'>Projects</button>
+          </div>
+          <p className='text-black text-lg mb-6 lg:text-xl'>{bioIntro}</p>
+          <p className='text-black text-lg mb-6 lg:text-xl'>{bioBackground}</p>
+        </div>
+      </div>
     </section>
   );
 }
