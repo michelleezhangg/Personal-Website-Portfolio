604a059a8937ed43d336f7cb532b5a45edaec7f2
Michelle Zhang <mizhang@chapman.edu>
2024-10-12 12:02:06 -0700

    Update constants to be on one file

--- src/app/components/ContactMeSection.jsx
+++ src/app/components/ContactMeSection.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ContactMeSection = () => {
   return (
-    <section>
-        ContactMeSection
+    <section className='bg-lightblue'>
+      <div className='flex flex-col items-center'>
+        <h1 className='title text-6xl p-10 pt-20 font-black'>Contact Me</h1>
+      </div>
     </section>
   );
 }
