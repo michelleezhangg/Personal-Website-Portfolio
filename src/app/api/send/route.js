9e0318c63288f8e83da586ab45765665c850b3f0
Michelle Zhang <mizhang@chapman.edu>
2024-10-14 13:07:52 -0700

    Remove unnecessary fonts and change layout file

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,18 +1,14 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
-import {
-  EMAIL as email,
-} from '../../constants/constants';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: `Michelle <${email}>`,
-      to: [email],
+      from: fromEmail,
+      to: [fromEmail],
       subject: 'Hello world',
-      // react: EmailTemplate({ firstName: 'John' }),
       react: (
         <>
           <p>Email Body</p>
