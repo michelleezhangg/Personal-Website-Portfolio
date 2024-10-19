b57fd2bcd489974c859ee77ae31eef11521aede4
Michelle Zhang <mizhang@chapman.edu>
2024-10-18 23:35:16 -0700

    Adjust spacing in Hero Section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -12,9 +12,9 @@ import LinkedInIcon from '../../../public/assets/linkedin-icon.svg';
 const HeroSection = () => {
   return (
     <section id='home' className='section bg-lightblue'>
-      <div className='flex p-10'>
+      <div className='flex p-6'>
         {/* Left Side: Profile Card */}
-        <div className='bg-blue p-10 pb-0 m-3 shadow-xl flex flex-col items-center'>
+        <div className='bg-blue p-10 pb-0 m-3 mt-0 shadow-xl flex flex-col items-center'>
           <Image
             src='/images/profile.png'
             alt='hero image'
@@ -30,6 +30,7 @@ const HeroSection = () => {
             <p className='text-sm font-black'>Email</p>
             <p className='flex justify-center text-sm'>{EMAIL}</p>
           </div>
+          {/* Social Media Icons */}
           <div className='flex flex-row gap-6 bg-white self-stretch -mx-10 mt-8 py-2 items-center justify-center'>
             <Link href={LINKS.linkedin}>
               <Image
@@ -58,8 +59,8 @@ const HeroSection = () => {
           </div>
         </div>
         {/* Right Side: Introduction Card */}
-        <div className='p-10 pt-16'>
-          <h1 className='title mb-4 text-6xl'>{NAME}</h1>
+        <div className='p-10'>
+          <h1 className='title my-4 text-6xl'>{NAME}</h1>
           <h3 className='text-3xl font-semibold'>{ROLE}</h3>
           <div>
             <button className='button blue-button px-8 mr-4'>
