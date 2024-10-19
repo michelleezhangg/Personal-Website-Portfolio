f188f700b12d279945f4af807043484a5530737e
Michelle Zhang <mizhang@chapman.edu>
2024-10-18 18:31:52 -0700

    Add maximum height requirements for the message resize

--- src/app/components/ContactMeSection.jsx
+++ src/app/components/ContactMeSection.jsx
@@ -129,7 +129,7 @@ const ContactMeSection = () => {
                   type='message'
                   id='message'
                   required
-                  className='bg-white border border-black text-sm rounded-lg block w-full p-2.5 resize-vertical min-h-[42px]'
+                  className='bg-white border border-black text-sm rounded-lg block w-full p-2.5 resize-vertical min-h-[42px] max-h-[400px]'
                   placeholder={MESSAGE_PLACEHOLDER}
                 ></textarea>
               </div>
