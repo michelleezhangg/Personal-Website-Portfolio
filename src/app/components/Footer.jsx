ec40d06ad0a97b89eb5328e16a0a447c9e682859
Michelle Zhang <mizhang@chapman.edu>
2024-10-20 11:45:37 -0700

    Rebase to fix buttons branch and resolved merge conflicts

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <footer className='bg-lightblue py-3'>
       <div className='flex flex-row'>
         <Link href={'#'}>
-          <h1 className='title text-xl pl-5 pb-2'>{NAME}</h1>
-          <h3 className='text-xs font-light pl-5 uppercase'>{ROLE}</h3>
+          <h1 className='title text-2xl pl-5 pb-2'>{NAME}</h1>
+          <h3 className='text-sm font-light pl-5 uppercase'>{ROLE}</h3>
         </Link>
         <p className='ml-auto mt-auto text-slate-400'>{COPYRIGHT}</p>
       </div>
