53354c402b7dee913ff88c9fa7246ea510c52ba2
Michelle Zhang <mizhang@chapman.edu>
2024-10-21 15:11:04 -0700

    Update route.js with testing filler values

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,15 +1,27 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
+import { CONTACT } from '@/app/constants';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
   try {
+    const { body } = await req.json();
+    const { email, subject, message } = body;
+
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
+      // from: fromEmail,
+      to: ['delivered@resend.dev', email],
+      subject: subject,
+      react: (
+        <div>
+          <h1>{subject}</h1>
+          <p>{CONTACT.messages.thank_you}</p>
+          <p>I will try to get back to you as soon as I can.</p>
+          <p>New message submitted: {message}</p>
+        </div>
+      ),
     });
 
     if (error) {
