4fc415516c631259eb061a7e2452b57a1d1295cb
Michelle Zhang <mizhang@chapman.edu>
2024-10-21 15:38:06 -0700

    Removed MenuOverlay file for simplicity

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,12 +2,15 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
-import MenuOverlay from './MenuOverlay';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import { PERSONAL, NAV_LINKS } from '../constants';
 
 const Navbar = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className='bg-lightblue sticky top-0'>
@@ -18,12 +21,12 @@ const Navbar = () => {
         </Link>
         {/* Mobile Menu */}
         <div className='block md:hidden'>
-          {!navbarOpen ? (
-              <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-1'>
+          {!isMenuOpen ? (
+              <button onClick={toggleMenu} className='flex items-center px-3 py-1'>
                 <Bars3Icon className='h-8 w-8' />
               </button>
             ) : (
-              <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-1'>
+              <button onClick={toggleMenu} className='flex items-center px-3 py-1'>
                 <XMarkIcon className='h-8 w-8' />
               </button>
           )}
@@ -49,7 +52,29 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={NAV_LINKS} /> : null}
+      {/* Mobile Menu Overlay */}
+      {isMenuOpen && (
+        <div className='md:hidden'>
+          <ul className='flex flex-col py-4 items-center'>
+            {NAV_LINKS.map((link, index) => (
+              <li key={index}>
+                <ScrollLink
+                  to={link.path}
+                  spy={true}
+                  smooth={true}
+                  offset={-200} // Extend spy region up 200px
+                  duration={500}
+                  className='block py-2 pr-4 hover:text-darkblue hover:cursor-pointer uppercase'
+                  activeClass='text-darkblue'
+                  onClick={toggleMenu}
+                >
+                  {link.title}
+                </ScrollLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
