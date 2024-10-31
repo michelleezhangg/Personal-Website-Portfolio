bdc3f7e3cedc96a8b2c400649c7f639ae75c0977
Michelle Zhang <mizhang@chapman.edu>
2024-10-31 15:50:03 -0700

    Completed MenuOverlay test file and all tests passed, may need to check highlighted section later

---
+++ src/tests/components/MenuOverlay.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MenuOverlay from '@/app/components/MenuOverlay';
+
+jest.mock('react-scroll', () => ({
+  Link: (props) => <a {...props} data-testid='scroll-link'>{props.children}</a>,
+}));
+
+const mockLinks = [
+  { title: 'Title 1', path: 'path1' },
+  { title: 'Title 2', path: 'path2' },
+  { title: 'Title 3', path: 'path3' },
+  { title: 'Title 4', path: 'path4' },
+];
+
+describe('MenuOverlay Component', () => {
+  it('renders MenuOverlay component with correct links', () => {
+    render(<MenuOverlay links={mockLinks} />);
+
+    mockLinks.forEach(link => {
+      expect(screen.getByText(link.title)).toBeInTheDocument();
+    });
+  });
+
+  it('renders the correct number of links', () => {
+    render(<MenuOverlay links={mockLinks} />);
+    
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems).toHaveLength(mockLinks.length);
+  });
+
+  it('link elements have correct attributes', () => {
+    render(<MenuOverlay links={mockLinks} />);
+
+    const scrollLinks = screen.getAllByTestId('scroll-link');
+    scrollLinks.forEach((link, index) => {
+      expect(link).toHaveTextContent(mockLinks[index].title);
+      expect(link).toHaveAttribute('to', mockLinks[index].path);
+    });
+  });
+});
