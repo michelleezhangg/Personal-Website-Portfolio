41a62a89a22d7078a0321c31d956338082cda663
Michelle Zhang <mizhang@chapman.edu>
2024-11-11 14:15:44 -0800

    Fix unit tests for ExperienceItem and ToolListItem

--- src/tests/components/ToolListItem.test.js
+++ src/tests/components/ToolListItem.test.js
@@ -15,15 +15,13 @@ const mockToolItem = {
 };
 
 describe("ToolListItem component", () => {
-  beforeEach(() => {
-    render(<ToolListItem {...mockToolItem} />);
-  });
-
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("renders tool icon with correct src and alt text", () => {
+  it("renders tool icon with correct src, alt text, and size when isMd is true", () => {
+    render(<ToolListItem {...mockToolItem} isMd={true} />);
+
     const iconImage = screen.getByAltText(`${mockToolItem.name} Icon`);
     expect(iconImage).toBeInTheDocument();
     expect(iconImage).toHaveAttribute("src", mockToolItem.path);
@@ -31,7 +29,18 @@ describe("ToolListItem component", () => {
     expect(iconImage).toHaveAttribute("height", "40");
   });
 
+  it("renders tool icon with correct src and alt text, and size when isMd is false", () => {
+    render(<ToolListItem {...mockToolItem} isMd={false} />);
+
+    const iconImage = screen.getByAltText(`${mockToolItem.name} Icon`);
+    expect(iconImage).toBeInTheDocument();
+    expect(iconImage).toHaveAttribute("src", mockToolItem.path);
+    expect(iconImage).toHaveAttribute("width", "30");
+    expect(iconImage).toHaveAttribute("height", "30");
+  });
+
   it("renders tool name correctly", () => {
+    render(<ToolListItem {...mockToolItem} isMd={true} />);
     expect(screen.getByText(mockToolItem.name)).toBeInTheDocument();
   });
 });
